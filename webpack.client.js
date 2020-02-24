6d2371f996572c22383017d98f91df3b993bed3e
Dmitry Anisimov <advgreat@yandex.ru>
2020-02-25 01:11:15 +0700

    +

--- webpack.client.js
+++ webpack.client.js
@@ -12,7 +12,12 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/index.html"
-    })
+    }),
+
+    new HtmlWebpackPlugin({
+      template: "./src/index2.html"
+    }),
+
   ],
   devServer: {
     proxy: {
